7cfaf07a230503734a511f0651c5e1ed9df52806
Sam Stratter <humanHardDrive@gmail.com>
2020-08-24 21:16:43 -0400

    Create a map to generate dialogs

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 #include <wx/msgdlg.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+std::map<std::string, std::function<int(const std::string&)>> dialogGenMap =
+{
+	{
+		"OK",
+		[](const std::string& sText)
+		{
+			return wxMessageDialog(nullptr, sText, "").ShowModal();
+		}
+	},
+	{
+		"YesNo", 
+		[](const std::string& sQuestion)
+		{
+			return wxMessageDialog(nullptr, sQuestion, "", wxYES_NO).ShowModal();
+		}
+	}
+};
 
 int main(int argc, char** argv)
 {
-	wxMessageDialog dlg(nullptr, "HELLO", wxMessageBoxCaptionStr, wxOK);
-	dlg.ShowModal();
+	boost::property_tree::ptree pt;
+
+	if (argc < 2)
+	{
+		std::cout << "No file specified" << std::endl;
+		return 1;
+	}
+
+	try
+	{
+		boost::property_tree::read_xml(argv[1], pt);
+	}
+	catch (boost::property_tree::ptree_error& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
 	return 0;
 }
