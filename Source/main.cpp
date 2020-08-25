809726bdf6e4f9785d51be127de1e053dafba290
Sam Stratter <humanHardDrive@gmail.com>
2020-08-24 21:36:34 -0400

    Traverse flowchart tree

--- Source/main.cpp
+++ Source/main.cpp
@@ -6,20 +6,28 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 
-std::map<std::string, std::function<int(const std::string&)>> dialogGenMap =
+std::map<std::string, std::function<std::string(const std::string&)>> dialogGenMap =
 {
 	{
 		"OK",
 		[](const std::string& sText)
 		{
-			return wxMessageDialog(nullptr, sText, "").ShowModal();
+			wxMessageDialog(nullptr, sText, "").ShowModal();
+			return "";
 		}
 	},
 	{
 		"YesNo", 
 		[](const std::string& sQuestion)
 		{
-			return wxMessageDialog(nullptr, sQuestion, "", wxYES_NO).ShowModal();
+			int nRes = wxMessageDialog(nullptr, sQuestion, "", wxYES_NO).ShowModal();
+
+			if (nRes == wxID_YES)
+				return "Yes";
+			else if (nRes == wxID_NO)
+				return "No";
+			else
+				return "";
 		}
 	}
 };
@@ -27,6 +35,7 @@ std::map<std::string, std::function<int(const std::string&)>> dialogGenMap =
 int main(int argc, char** argv)
 {
 	boost::property_tree::ptree pt;
+	boost::property_tree::ptree flowRoot;
 
 	if (argc < 2)
 	{
@@ -41,6 +50,42 @@ int main(int argc, char** argv)
 	catch (boost::property_tree::ptree_error& e)
 	{
 		std::cout << e.what() << std::endl;
+		return 1;
+	}
+
+	std::cout << "Description: " << pt.get<std::string>("Description", "") << std::endl;
+	std::cout << "Tags: " << pt.get<std::string>("Tags", "") << std::endl;
+
+	if (pt.get_child_optional("flow"))
+	{
+		bool bPathDefined = true;
+		flowRoot = pt.get_child("flow");
+
+		while (bPathDefined)
+		{
+			std::string sType = flowRoot.get<std::string>("Type", "");
+			std::string sRes = "";
+
+			bPathDefined = false;
+			if (dialogGenMap.find(sType) != dialogGenMap.end())
+				sRes = dialogGenMap[sType](flowRoot.get<std::string>("Text", ""));
+			else
+				dialogGenMap["OK"]("No dialog defined");
+
+			if (!sRes.empty() && flowRoot.get_child_optional(sRes))
+			{
+				bPathDefined = true;
+				flowRoot = flowRoot.get_child(sRes);
+			}
+
+			if (!bPathDefined)
+				dialogGenMap["OK"]("No path defined");
+		}
+
+	}
+	else
+	{
+		dialogGenMap["OK"]("No flow defined");
 	}
 
 	return 0;
